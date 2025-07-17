6a2436e7b1b1b1ca6da2371cf6a06edac3eafebf
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-07-17 11:44:02 -0300

    Add files via upload

--- M2-function-2/app.js
+++ M2-function-2/app.js
@@ -6,6 +6,7 @@ paragrafo.innerHTML = "Escolha um número entre 1 e 10";
  */
 
 let numeroSecreto = 5;
+let tentativas = 1
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag)
@@ -17,10 +18,32 @@ exibirTextoNaTela("p", "Escolha um numero entre 1 e 10");
 
 function verificarChute(){
     let chute = document.querySelector("input").value;
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        exibirTextoNaTela("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você acertou o numero secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela("p", mensagemTentativas);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela("p", "O numero secreto é menor");
+        }else{
+            exibirTextoNaTela("p", "O numero secreto é maior");
+        }
+        tentativas++;
+        limparCampo();
+    }
+    /* console.log(chute == numeroSecreto); */
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1);
 }
+
+function limparCampo(){
+     chute = document.querySelector('input');
+     chute.value = '';
+}
+
+
 
