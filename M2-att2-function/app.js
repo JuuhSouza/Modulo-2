f494849a45a08cb8aa170691897952f860e10421
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-07-17 11:07:45 -0300

    Add files via upload

---
+++ M2-att2-function/app.js
@@ -0,0 +1,46 @@
+/* 1 - Ola mundo */
+
+function olaMundo(){
+    let olaMundo = ("Olá mundo");
+    console.log(olaMundo);
+}
+olaMundo();
+
+/* 2 - Ola nome */
+
+function nome(nome){
+    console.log(`Olá ${nome}`);
+}
+nome("Júlia");
+
+/* 3 - multiplicação */
+
+function recebeNumero(numero, dobro){
+    console.log(numero * dobro);
+}
+recebeNumero(8,4);
+
+/* 4 - Média */
+
+function media(n1, n2, n3){
+    console.log((n1 + n2+ n3) / 3);
+}
+media(6, 8, 6);
+ 
+/* 5 - Maior nunero */
+
+function maior(n1, n2){
+    console.log(n1 > n2 ? n1 : n2);
+}
+maior(10, 4);
+
+/* 6 - Resultado multiplicação do msm numero */
+
+function recebe(n1){
+    console.log(n1 *n1);
+}
+recebe(9);
+
+
+
+
