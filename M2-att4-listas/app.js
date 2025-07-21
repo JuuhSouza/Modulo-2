07a81154638d2cfa824460be8b5bc4c8e3f5cd44
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-07-21 18:49:43 -0300

    Add files via upload

---
+++ M2-att4-listas/app.js
@@ -0,0 +1,30 @@
+/* 1 - lista vazia */
+
+let listaGenerica = [];
+
+/* 2 - linguagens De Programacao  */
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+/* 3 - linguagens De Programacao */
+let linguagensDeProgramacao2 = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+linguagensDeProgramacao.push("Java", "Ruby", "Golang");
+
+/* 4 - lista com 3 nomes */ 
+let nomes1 = ["caneta", "lápis", "borracha"];
+console.log(nomes1[0]);
+
+/* 5 - lista com 3 nomes */
+
+let nomes2 = ["caneta", "lápis", "borracha"];
+console.log(nomes1[1]);
+
+/* 6 - lista com 3 nomes */
+let nomes3 = ["caneta", "lápis", "borracha"];
+console.log(nomes1[2]);
+
+
+
+
+
+
+
